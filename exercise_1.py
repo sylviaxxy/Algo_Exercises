f97fa14d849d657c5307cbe820bab7ec7c388787
sylviaxxy <xuxinyusylvia@outlook.com>
2020-01-30 19:47:29 -0500

    20200130 commit

--- exercise_1.py
+++ exercise_1.py
@@ -1,12 +1,17 @@
-# Question 1: How to implement a queue by two stacks?
+# LC 20 Valid Parentheses
 
-def queue_pop(stack_1, stack_2):
-    while stack_1:
-        stack_1.pop()
-        stack_2.push()
+# WRONG
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in ['(', '[', '{' ]:
+                stack.append(c)
+            else:
+                top = stack.pop()
+                if (c == ')' and top != '(') or \
+                    (c == ']' and top != '[') or \
+                    (c == '}' and top != '{'):
+                    return False
+        return not stack
 
-    print(stack_2.pop())
-
-# Question 2: How to implement the min() function when using stack with time complexity O(1)?
-def stack_min(stack_1, stack_2):
-    
--- exercise_3.py
+++ exercise_3.py
@@ -1,2 +1,61 @@
-# Insert Sort List: 
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class ListNode(object):
+
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+# LC173: Insertion Sort List: 
 # Sort a linked list using insertion sort
+    def insertionSortList(self, head):
+        dummy = ListNode(0)
+        cur = head
+        while cur is not None:
+            pre = dummy
+            # scan from the beginning of sorted part of this LinkedList, 
+            # if find appropriate positon for cur, insert cur between pre and prev.next
+            # otherwise keep scaning
+            while pre.next is not None and pre.next.val < cur.val:
+                pre = pre.next
+            temp = cur.next
+            cur.next = pre.next
+            pre.next = cur
+            cur = temp
+        
+        return dummy.next
+
+# LC206. Reverse Linked List
+    def reverseLinkedList_iter(self, head):
+        if head is None or head.next is None:
+            return head
+        else:
+            pre = None #if pre = ListNode(None) then can not pass test case
+            cur = head
+            while cur is not None:
+                temp = cur.next
+                cur.next = pre
+                pre = cur
+                cur = temp
+            return pre
+    
+    def reverseLinkedList_recursive(self, head):
+        # base case
+        if head is None or head.next is None:
+            return head
+        else:
+        # use itself
+            new_head = self.reverseLinkedList_recursive(head.next)
+            head.next.next = head
+            head.next = None
+            return new_head
+    
